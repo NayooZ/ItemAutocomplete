b73d06492f390cb215b6cb29301aeb23208b41ab
NayooZ <37068647+NayooZ@users.noreply.github.com>
2024-05-05 19:55:07 +1000

    Update ItemDatabase.lua
    Cata update for new item range

--- Source/ItemDatabase.lua
+++ Source/ItemDatabase.lua
@@ -8,7 +8,7 @@ local utf8 = require 'Shared.UTF8'
 local const = util.ReadOnly({
   -- See: https://tbc.wowhead.com/items?filter=151;1;187815
   itemIds = util.IsWotlk() and {
-    { 1, 54798 }, -- Defaults
+    { 1, 78910 }, -- Defaults
     { 122270 }, -- WoW Token (AH)
     { 122284 }, -- WoW Token
     { 172070 }, -- Customer Service Package
